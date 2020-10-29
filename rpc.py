e46379810c9580eaa0469163e6380779f0b06c20
TheMaksoo <39918032+Moheax@users.noreply.github.com>
2020-10-29 17:43:55 +0100

    Update rpc.py

--- rpc.py
+++ rpc.py
@@ -6,7 +6,7 @@ def play():
     user = input("What's your choice? 'r' for rock, 'p' for paper, 's' for scissors\n")
     user = user.lower()
 
-    computer = random.choice(['r', 'p', 's'])
+    computer = random.randchoice(['r', 'p', 's'])
 
     if user == computer:
          return (0, user, computer)
